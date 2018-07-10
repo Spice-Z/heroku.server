397dd892f1f73cae32ce7eaaa0e8482dcf824b21
小倉優悟 <jigenbakdaaan@gmail.com>
2018-07-11 00:35:12 +0900

    fix

--- pg-setting.js
+++ pg-setting.js
@@ -4,14 +4,13 @@ const options = {
 const pgp = require("pg-promise")(options);
 
 const connection = {
-  host: "ec2-54-243-63-13.compute-1.amazonaws.com",
-  user: "unwzsfdqhpohef",
-  password: "e95f2b0400ed20c9c6753fc26627af7db8803524c2f0c2ffa5c5ae1bed34706f",
-  database: "d2t59hbqkikfu4",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER_NAME,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
   port: 5432,
   ssl: true
 };
 
 const db = pgp(connection);
-
 module.exports = db;
